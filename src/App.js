1a71bfa2732454633fbc7e8b562037088bfad1cf
anil sidhu <webanilsidhu@gmail.com>
2020-11-07 00:48:47 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {User} from './User'
 function App() {
+
   return (
     <div className="App">
      <h1>Hello World !</h1>
+     <User />
     </div>
   );
 }
 
+
+
 export default App;
+
+
--- src/User.js
+++ src/User.js
@@ -1,10 +1,6 @@
-function User() {
-    return (
-      <div className="App">
-       <h1>Hello User !</h1>
-      </div>
-    );
-  }
-  
-  export default User;
-  
+export function User()
+{
+    return(
+        <h1>User</h1>
+    )
+}
