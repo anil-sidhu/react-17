7185a188bbf85887bc76d64536f91ae0c85df8a4
anil sidhu <webanilsidhu@gmail.com>
2020-11-07 23:47:02 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data,setData]=useState(0)
+  function updateData()
+  {
+    setData(data+1)
+   
+  }
+  console.warn("______");
   return (
     <div className="App">
-     <h1>Hello World !</h1>
+     <h1>{data}</h1>
+     <button onClick={updateData}>Update Data</button>
     </div>
   );
 }
