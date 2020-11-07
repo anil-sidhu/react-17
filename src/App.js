fc26f15e19f42382ef31ae28f53e8baa06ec6bbf
anil sidhu <webanilsidhu@gmail.com>
2020-11-07 19:58:42 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  let data="anil sidhu";
+  function apple()
+  {
+  data="peter";
+    alert(data);
+  }
   return (
     <div className="App">
-     <h1>Hello World !</h1>
+     <h1>{data}</h1>
+     <button onClick={apple}>Click Me</button>
     </div>
   );
 }
