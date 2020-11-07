113be9d782998c7afa26d5c8526081099005d447
anil sidhu <webanilsidhu@gmail.com>
2020-11-08 01:30:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React,{Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
+class  App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello World !</h1>
-    </div>
-  );
+  constructor()
+  {
+    super();
+    this.state={
+      data:1
+    }
+  }
+  apple()
+  {
+    this.setState({data:this.state.data+1})
+  }
+  render()
+  {
+    return (
+      <div className="App">
+       <h1>{this.state.data}</h1>
+       <button onClick={()=>this.apple()}>Update Data</button>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
