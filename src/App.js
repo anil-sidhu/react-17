3df1420d1ae30d991cb0ddc444c19f89ba0bd81f
anil sidhu <webanilsidhu@gmail.com>
2020-11-08 16:51:37 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React,{useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
-
+import Student from './Student'
 function App() {
+  const [name,setName]=useState("Anil")
+  function apple()
+  {
+    alert("hello")
+  }
   return (
     <div className="App">
-     <h1>Hello World !</h1>
+     <h1>Props in React :)</h1>
+     <Student name={name}/>
+     <button onClick={()=>{setName("Sidhu")}} >Update Name</button>
     </div>
   );
 }
---
+++ src/Student.js
@@ -0,0 +1,10 @@
+function Student(props) {
+    console.log(props)
+    return (
+        <div style={{ backgroundColor: "skyblue", margin: 10 }}>
+            <h1>Hello {props.name}</h1>
+        </div>
+    )
+}
+
+export default Student
