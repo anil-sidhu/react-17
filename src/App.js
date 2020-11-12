3f7ef3f782750f51465d750363669bd5938fc5bf
anil sidhu <webanilsidhu@gmail.com>
2020-11-12 22:07:04 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react'
 function App() {
+  const [show,setShow]=useState(true)
   return (
     <div className="App">
-     <h1>Hello World !</h1>
+     {
+       show?<h1>Hello World !</h1>:null
+     }
+     {/* <button onClick={()=>setShow(true)} >Show</button>
+     <button onClick={()=>setShow(false)} >Hide</button> */}
+<button onClick={()=>setShow(!show)} >Toggle</button> 
     </div>
   );
 }
