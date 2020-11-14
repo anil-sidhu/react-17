d1b500fa4f5f53b77d29c44b45d44f3c40385e1c
anil sidhu <webanilsidhu@gmail.com>
2020-11-15 00:12:06 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello World !</h1>
+import React from 'react'
+class App extends React.Component{
+  constructor()
+  {
+    super();
+    this.state={
+      name:"Anil sidhu"
+    }
+    console.warn("constructor")
+  }
+  render()
+  {
+    console.warn("render")
+    return<div>
+      <h1>Hello World {this.state.name}</h1>
     </div>
-  );
+  }
 }
-
 export default App;
