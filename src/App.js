3c46b5440405e021f53d243f3ac3dbdadc28ae62
anil sidhu <webanilsidhu@gmail.com>
2020-11-17 22:50:38 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+class App extends React.Component {
+  constructor()
+  {
+    super();
+    console.warn("constructor")
+    this.state={name:"anil"}
+  }
+  componentDidMount()
+  {
+    console.warn("componentDidMount")
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello World !</h1>
-    </div>
-  );
+  }
+  render()
+  {
+    console.warn("render")
+
+    return (
+      <div className="App">
+        <h1>Component Did Mount {this.state.name}</h1>
+        <button onClick={()=>this.setState({name:"Sidhu"})}>Update</button>
+      </div>
+    );
+  }
 }
 
 export default App;
