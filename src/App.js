0c351ee4cb19d1f63c4492be6a365a3e5353a273
anil sidhu <webanilsidhu@gmail.com>
2020-11-20 18:04:14 +0530

    shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      count:0
+    }
+  }
+  shouldComponentUpdate()
+  {
+    console.warn("shouldComponentUpdate",this.state.count)
+   if(this.state.count<5)
+   {
+     return true;
+   }
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello World !</h1>
-    </div>
-  );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Should Component Update  {this.state.count}</h1>
+        <button onClick={()=>this.setState({count:this.state.count+1})}>Update Counter</button>
+      </div>
+    );
+  }
 }
 
 export default App;
