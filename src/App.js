3006bdedfbb55c28ac55970513b3eb61e70fe2ee
anil sidhu <webanilsidhu@gmail.com>
2020-11-20 23:37:00 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState}from 'react'
 function App() {
+  const [data,setData]=useState("Anil")
   return (
     <div className="App">
-     <h1>Hello World !</h1>
+      <h1>Hooks in React {data}</h1>
+      <button onClick={()=>setData("Peter")}>Update State</button>
     </div>
   );
 }
