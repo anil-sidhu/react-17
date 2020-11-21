33be602e8818e2e333f72e80f921d04252a717e6
anil sidhu <webanilsidhu@gmail.com>
2020-11-22 01:54:28 +0530

    done

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ function App() {
       <h1 className="primary">Style type 1 in React js</h1>
       <h1 style={styleItem}>Style type 2 in React js</h1>
       <h1 className={style.success} >Style type 3 in React js</h1>
-
-
     </div>
   );
 }
