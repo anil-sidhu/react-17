33fa3f5b5036fb51952c565d4e27e38b46daa835
anil sidhu <webanilsidhu@gmail.com>
2020-11-22 02:46:05 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import React,{useState}from 'react'
+import React from 'react'
+import {Button,Badge,Card} from 'react-bootstrap'
 function App() {
-  const [data,setData]=useState("Anil")
   return (
     <div className="App">
-      <h1>Hooks in React {data}</h1>
-      <button onClick={()=>setData("Peter")}>Update State</button>
+      <h1>Install Bootstrap</h1>
+      <h1>
+    Example heading <Badge variant="light">New</Badge>
+  </h1>
+
+  <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
