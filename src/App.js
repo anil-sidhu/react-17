467d45af4092b7748cb6c9a2a577082b54a7e1b1
anil sidhu <webanilsidhu@gmail.com>
2020-11-24 22:20:14 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
-import React,{useState}from 'react'
+import React from 'react'
+import {Table} from 'react-bootstrap'
 function App() {
-  const [data,setData]=useState("Anil")
+  const users = [
+    { name: 'Anil', email: 'anil@test.com', contact: '111' },
+    { name: 'Burce', email: 'bruce@test.com', contact: '222' },
+    { name: 'Peter', email: 'peter@test.com', contact: '111' },
+    { name: 'Sam', email: 'sam@test.com', contact: '777' },
+  ]
   return (
     <div className="App">
-      <h1>Hooks in React {data}</h1>
-      <button onClick={()=>setData("Peter")}>Update State</button>
+      <h1>List With Bootstrap Table</h1>
+      <Table striped variant="dark" >
+        <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Contacts</td>
+        </tr>
+        {
+          users.map((item,i)=>
+          item.contact==='111'?
+          <tr key={i}>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+        <td>{item.contact}</td>
+        </tr>:null
+          )
+        }
+        </tbody>
+      </Table>
     </div>
   );
 }
