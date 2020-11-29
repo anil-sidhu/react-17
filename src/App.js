94abc2b6caf71d0eb3f5151bda66b7583b6691d4
anil sidhu <webanilsidhu@gmail.com>
2020-11-29 20:29:20 +0530

    done use memo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import React,{useState}from 'react'
+import React, { useState } from 'react'
 function App() {
-  const [data,setData]=useState("Anil")
+  const [count, setData] = useState(1)
+  const [item, setItem] = useState(20)
+
+  const newApple=React.useMemo(
+    function appleTime() {
+      console.warn("Hello")
+      return 100 * count;
+    }
+  ,[item])
   return (
     <div className="App">
-      <h1>Hooks in React {data}</h1>
-      <button onClick={()=>setData("Peter")}>Update State</button>
+      <h1>Hooks in React {count}</h1>
+      {newApple}
+      <button onClick={() => setData(count + 1)}>Update State</button>
+      <button onClick={() => setItem(item * 10)}>Update State</button>
+
     </div>
   );
 }
