97530560f7886abf3c37695d8a0a5eb9eae21223
anil sidhu <webanilsidhu@gmail.com>
2020-12-03 19:29:17 +0530

    done ref code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
-import React,{useState}from 'react'
-function App() {
-  const [data,setData]=useState("Anil")
-  return (
-    <div className="App">
-      <h1>Hooks in React {data}</h1>
-      <button onClick={()=>setData("Peter")}>Update State</button>
-    </div>
-  );
+import React,{createRef} from 'react'
+class App extends React.Component {
+  constructor()
+  {
+    super();
+    this.inputRef=createRef();
+  }
+  componentDidMount()
+  {
+    // console.warn(this.inputRef.current.value="1000")
+  }
+  getVal()
+  {
+    console.warn(this.inputRef.current.value)
+    this.inputRef.current.style.color="red"
+    this.inputRef.current.style.backgroundColor="black"
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Ref in React </h1>
+        <input type="text" ref={this.inputRef}  />
+        <button onClick={()=>this.getVal()}>Check Ref</button>
+      </div>
+    );
+  }
 }
 
 export default App;
