82aa1cfb17eed711f04958b720b711aefe916e86
anil sidhu <webanilsidhu@gmail.com>
2020-12-05 12:58:49 +0530

    uncontrolled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import React,{useState} from 'react'
+import React, { useState, useRef } from 'react'
 function App() {
-  const [val,setVal]=useState("")
-
-   return (
+  let nameRef = useRef(null)
+  function submit(e) {
+    e.preventDefault()
+    let a = document.getElementById('name').value
+    console.warn("a[[le", a, nameRef.current.value)
+  }
+  return (
     <div className="App">
       <h1>Controlled Component </h1>
-      <input type="text" value={val} onChange={(e)=>setVal(e.target.value)} />
-   <h1>{val}</h1>
+      <form onSubmit={submit} >
+        <input type="text" id="name" /> <br /><br />
+        <input ref={nameRef} type="text" /> <br /><br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 
 }
-export default  App;
+export default App;
