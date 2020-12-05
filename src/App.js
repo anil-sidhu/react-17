f2ce0409e670030cc644590526773a0828615d8a
anil sidhu <webanilsidhu@gmail.com>
2020-12-05 13:22:49 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
-import React, { useState, useRef } from 'react'
+import React, { useRef } from 'react'
 function App() {
-  let nameRef = useRef(null)
-  function submit(e) {
+  let inputRef = useRef(null)
+  let inputRef2 = useRef(null)
+
+  function submitForm(e) {
     e.preventDefault()
-    let a = document.getElementById('name').value
-    console.warn("a[[le", a, nameRef.current.value)
+    console.warn("input field 1 value : ", inputRef.current.value)
+    console.warn("input field 2 value : ", inputRef2.current.value)
+    let input3 = document.getElementById('input3').value
+    console.warn("input field 3 value : ", input3)
+
+
   }
   return (
     <div className="App">
-      <h1>Controlled Component </h1>
-      <form onSubmit={submit} >
-        <input type="text" id="name" /> <br /><br />
-        <input ref={nameRef} type="text" /> <br /><br />
+      <h1>Uncontrolled Component </h1>
+      <form onSubmit={submitForm} >
+        <input ref={inputRef} type="text" /> <br /> <br />
+        <input ref={inputRef2} type="text" /> <br /> <br />
+        <input id="input3" type="text" /> <br /> <br />
+
         <button>Submit</button>
       </form>
     </div>
